7a75aec2d9670df33ae88160a825ddb9a6a7fb74
Max1O <fcb10max@gmail.com>
2021-11-09 13:47:24 +0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -25,25 +25,31 @@ function App() {
   const [menuActive, setMenuActive] = useState(false);
   const [dispFixed, setDispFixed] = useState(false);
   const [mainActive, setMainActive] = useState(true);
-  const [catalog, setCatalog] = useState('');
+  const [catalog, setCatalog] = useState("");
 
   const Container = styled.div`
     position: ${(props) => (props.fixed ? "fixed" : "static")};
   `;
 
+  const urls = ["/login", "/support", "/categories"];
+
   return (
     <Container fixed={dispFixed}>
       {mainActive && (
         <BrowserRouter>
-          <TopNavbar
-            setMenuActive={setMenuActive}
-            setDispFixed={setDispFixed}
-          />
-          <BigMenu
-            active={menuActive}
-            setMenuActive={setMenuActive}
-            setDispFixed={setDispFixed}
-          />
+          {!urls.includes(window.location.pathname) && (
+            <>
+              <TopNavbar
+                setMenuActive={setMenuActive}
+                setDispFixed={setDispFixed}
+              />
+              <BigMenu
+                active={menuActive}
+                setMenuActive={setMenuActive}
+                setDispFixed={setDispFixed}
+              />
+            </>
+          )}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/features" element={<Features />} />
@@ -56,57 +62,24 @@ function App() {
             <Route path="/stream" element={<Streams />} />
             <Route path="/login" element={<Login />} />
             <Route path="/downloadNow" element={<DownloadNow />} />
-          </Routes>
-          <Footer
-            devices={devices}
-            setDevice={setDevice}
-            setMainActive={setMainActive}
-          />
-        </BrowserRouter>
-      )}
-      {!mainActive && (
-        <BrowserRouter>
-          <Routes>
             <Route
               path="/support"
-              element={<Support setMainActive={setMainActive} setCatalog={setCatalog} />}
+              element={
+                <Support
+                  setMainActive={setMainActive}
+                  setCatalog={setCatalog}
+                />
+              }
             />
             <Route path="/categories" element={<Categories data={catalog} />} />
           </Routes>
-          <div
-            style={{
-              width: "100vw",
-              display: "flex",
-              justifyContent: "center",
-              margin: "40px 0",
-              borderBottom: "1px solid gray",
-              paddingBottom: "40px",
-            }}
-          >
-            <p
-              style={{
-                fontSize: "40px",
-                fontWeFight: 700,
-                width: "90vw",
-                textAlign: "center",
-              }}
-            >
-              Can’t find what you’re looking for?{" "}
-              <span style={{ color: "#fb800d", cursor: "pointer" }}>
-                Contact us
-              </span>
-            </p>
-          </div>
-          <p
-            style={{
-              fontSize: "12px",
-              textAlign: "center",
-              color: "gray",
-              marginBottom: "20px",
-            }}
-          >
-            UltraVPN
-          </p>
+          {!urls.includes(window.location.pathname) && (
+            <Footer
+              devices={devices}
+              setDevice={setDevice}
+              setMainActive={setMainActive}
+            />
+          )}
         </BrowserRouter>
       )}
     </Container>
--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -12,6 +12,40 @@ const Support = ({ setMainActive, setCatalog }) => {
     <>
       <Header setMainActive={setMainActive} />
       <Catalog data={data} setCatalog={setCatalog} />
+      <div
+            style={{
+              width: "100vw",
+              display: "flex",
+              justifyContent: "center",
+              margin: "40px 0",
+              borderBottom: "1px solid gray",
+              paddingBottom: "40px",
+            }}
+          >
+            <p
+              style={{
+                fontSize: "40px",
+                fontWeFight: 700,
+                width: "90vw",
+                textAlign: "center",
+              }}
+            >
+              Can’t find what you’re looking for?{" "}
+              <span style={{ color: "#fb800d", cursor: "pointer" }}>
+                Contact us
+              </span>
+            </p>
+          </div>
+          <p
+            style={{
+              fontSize: "12px",
+              textAlign: "center",
+              color: "gray",
+              marginBottom: "20px",
+            }}
+          >
+            UltraVPN
+          </p>
     </>
   );
 };
